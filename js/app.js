572b4c9a84ce39f5fdfe7ef628a16d527f1819fc
junjing02 <junjingting@gmail.com>
2026-08-11 23:00:21 +0800

    Make the bow color swatch match the app's icon-button style
    Was a 32px square block that didn't match anything else on screen.
    Restyled to a 22px circle, same size/shape as the app's existing
    small icon buttons (help, settings, close, etc.), and vertically
    centered in the buy-actions row instead of top-aligned.

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ import {
   pickRandomSpecies,
   STAGE_MOVE_DURATION_S,
   STAGE_WANDER_INTERVAL_MS,
-} from "./pet-sprites.js?v=27";
-import * as db from "./supabase.js?v=27";
-import { playSound, soundEnabled, setSoundEnabled } from "./sound.js?v=27";
+} from "./pet-sprites.js?v=28";
+import * as db from "./supabase.js?v=28";
+import { playSound, soundEnabled, setSoundEnabled } from "./sound.js?v=28";
 
 const HOUR = 3600000;
 
