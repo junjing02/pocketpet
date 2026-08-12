c033a9c1cd11ee067fa5ec4f976a378fe930b773
junjing02 <junjingting@gmail.com>
2026-08-12 12:21:47 +0800

    Make the bed oval again, drop the pillow accent
    Kept the flat fill + 1px line from the last restyle (no gradient/shadow), just changed the shape back to an oval mat and removed the pillow square.

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ import {
   pickRandomSpecies,
   STAGE_MOVE_DURATION_S,
   STAGE_WANDER_INTERVAL_MS,
-} from "./pet-sprites.js?v=40";
-import * as db from "./supabase.js?v=40";
-import { playSound, soundEnabled, setSoundEnabled } from "./sound.js?v=40";
+} from "./pet-sprites.js?v=41";
+import * as db from "./supabase.js?v=41";
+import { playSound, soundEnabled, setSoundEnabled } from "./sound.js?v=41";
 
 const HOUR = 3600000;
 
